6169f00ebe74deaa766812b450310b18d035eb31
BenG1994 <113778995+BenG1994@users.noreply.github.com>
2023-02-06 10:30:09 -0600

    Actual finished project

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -2,119 +2,101 @@
 //  CategoryViewController.swift
 //  Todoey
 //
-//  Created by Ben Gauger on 11/20/22.
-//  Copyright © 2022 App Brewery. All rights reserved.
-//
 
 import UIKit
-import CoreData
+import RealmSwift
 
-class CategoryViewController: UITableViewController {
+
+class CategoryViewController: SwipeTableViewController {
     
-    var categories = [Category]()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let realm = try! Realm()
+
+    var categories: Results<Category>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         loadCategories()
-        
     }
+
     
-    //MARK: - TableView Datasource Methods
-    
+    //Mark: - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        cell.textLabel?.text = categories[indexPath.row].name
-        
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories added yet"
+    
         return cell
     }
     
     
-    //MARK: - TableView Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "goToItems", sender: self)
-        
-        
-    }
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationVC = segue.destination as! ToDoListViewController
-        
-        if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
-        }
-    }
-    
-    
-    //MARK: - Data Manupulation Methods
-    
-    func saveCategories() {
-        do{
-            try context.save()
-        }catch {
-            print ("Error")
+    //Mark: - Data Manipulation Methods
+    func save(category: Category) {
+        do {
+            try realm.write {
+                realm.add(category)
+            }
+        } catch {
+            print("Error saving category \(error)")
         }
         tableView.reloadData()
-        
     }
     
     func loadCategories() {
         
-        let request: NSFetchRequest<Category> = Category.fetchRequest()
-        
-        
-        do {
-            categories = try context.fetch(request)
-        }catch {
-            print ("error")
-        }
-        
+        categories = realm.objects(Category.self)
         tableView.reloadData()
-        
     }
     
+    //Mark: - Delete Data from Swipe
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
     
-    
+    //Mark: - Add New Categories
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
-        
-        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
-        
-        let action = UIAlertAction(title:"Add", style: .default) { (action) in
-            
-            
-            let newCategory = Category(context: self.context)
+        let alert = UIAlertController(title: "Add a New Cateogry", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newCategory = Category()
             newCategory.name = textField.text!
-            
-            
-            self.categories.append(newCategory)
-            
-            
-            self.saveCategories()
-            
-            
+            self.save(category: newCategory)
         }
         
         alert.addAction(action)
-        
         alert.addTextField { (field) in
             textField = field
             textField.placeholder = "Add a new category"
-            
         }
-        
         present(alert, animated: true, completion: nil)
-        
     }
+    
+    //Mark: - Tableview Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ToDoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories?[indexPath.row]
+        }
+    }
+    
+    
+    
 }
+
